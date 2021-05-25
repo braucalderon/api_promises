77223696bb2f1e7d13c32a2f2e619519cb1e95b9
Braulio <braumanhattan@gmail.com>
2021-05-25 00:27:34 -0400

    axios

--- src/component/getWeather.jsx
+++ src/component/getWeather.jsx
@@ -5,18 +5,35 @@ class GetWeather extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            data: [] // store data here after fetching
+            data: [], // store data here after fetching
+            state: 'new york'
         }
     }
 
     componentDidMount() {
+        const key = '';
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.state}&appid=${key}`;
         // fetch the api by using axios
-    }
+        axios.get(url)
+        .then(response => {
+            const res = response.data;
+            this.setState({data: res});
+            console.log(res);
+        })
+        .catch(error => {
+            console.log(error);
+        })
 
+    }
+   
     render() {
 
+        console.log(this.state.data);
+
         return(
-            <div> Checking </div>
+            <div>
+                <div></div>
+            </div>
         )
     }
 }
